b8bdd876fede96fd7642364c1f1ee0216e78109b
biaomac <biao.mac@gmail.com>
2013-06-24 23:17:47 +0900

    Update getter

--- DomainObjectAccessor.hpp
+++ DomainObjectAccessor.hpp
@@ -2,9 +2,9 @@
 #define ACCESSOR_HPP
 
 #define SETTER(name, Name, Type)  void set##Name(const Type &name)
-#define GETTER(name, Name, Type)  Type get##Name()
+#define GETTER(name, Name, Type)  Type get##Name() const
 
 #define SETTER_IMPL(Class, name, Name, Type) void Class::set##Name(const Type &name) { this->name = name; }
-#define GETTER_IMPL(Class, name, Name, Type) Type Class::get##Name() { return this->name; }
+#define GETTER_IMPL(Class, name, Name, Type) Type Class::get##Name() const { return this->name; }
 
 #endif // ACCESSOR_HPP
